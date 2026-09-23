b011909c7a7d86b0b8d167b5c00a4eb6b4138cd5
rht-rahat <contract.rahat@gmail.com>
2026-09-24 01:53:22 +0600

    re write sloved

--- config/email.js
+++ config/email.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
@@ -8,8 +9,19 @@ const transporter = nodemailer.createTransport({
   }
 });
 
+// Verify transporter connection
+transporter.verify((error, success) => {
+  if (error) {
+    console.error('Email transporter error:', error.message);
+  } else {
+    console.log('Email transporter ready');
+  }
+});
+
 const sendEmail = async ({ to, subject, html }) => {
   try {
+    console.log('Sending email to:', to);
+    console.log('Email user:', process.env.EMAIL_USER);
     const info = await transporter.sendMail({
       from: `"MCES International" <${process.env.EMAIL_USER}>`,
       to,
@@ -19,7 +31,7 @@ const sendEmail = async ({ to, subject, html }) => {
     console.log('Email sent:', info.messageId);
     return true;
   } catch (error) {
-    console.error('Email send error:', error);
+    console.error('Email send error:', error.message);
     return false;
   }
 };
